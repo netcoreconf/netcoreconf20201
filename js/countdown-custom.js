b97566934322813dcf46404f7477dd4a0270095e
Manuel Sanchez <manuss20@gmail.com>
2021-01-13 12:42:57 +0100

    update new web

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2021, 1, 27)}); // year, month, date, hour
+        });
+});		
+
