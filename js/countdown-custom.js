308714963016200cbe80dbb4c832f9d864565e18
Manuel Sanchez <manuss20@gmail.com>
2021-06-13 18:52:40 +0200

    Udpate new dates for second event online

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2021, 1, 27)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2021, 9, 9)}); // year, month, date, hour
         });
 });		
 
