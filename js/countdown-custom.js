c7cab269e61594905738541e3a8cd0f98301a08d
Robert Bermejo <bermejoblasco@users.noreply.github.com>
2022-08-21 20:15:01 +0200

    Update countdown-custom.js

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown('2022/09/17 09:00:00'); // year, month, date, hour
+            $('#defaultCountdown').countdown('2022/17/09 09:00:00'); // year, month, date, hour
         });
 });		
 
