a056016f689fada3a663d87ff0b5052fd1b0d10c
Robert Bermejo <bermejoblasco@live.com>
2023-01-29 12:27:17 +0100

    Netcoreconf BCN 2023

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-             $('#defaultCountdown').countdown({until: new Date(2022, 10, 18, 9)}); // year, month, date, hour
+             $('#defaultCountdown').countdown({until: new Date(2023, 02, 4, 9)}); // year, month, date, hour
         });
 });		
 
