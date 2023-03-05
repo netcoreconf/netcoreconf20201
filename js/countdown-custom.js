66f2a43119adec31ccca4caf7c7abc219adda10a
Robert Bermejo <bermejoblasco@live.com>
2023-03-05 18:22:12 +0100

    Update Malaga

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-             $('#defaultCountdown').countdown({until: new Date(2023, 02, 4, 9)}); // year, month, date, hour
+             $('#defaultCountdown').countdown({until: new Date(2023, 06, 9, 9)}); // year, month, date, hour
         });
 });		
 
