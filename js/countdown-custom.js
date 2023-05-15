5ef56a7079d78e94d46ce33346d819722588f115
Manuel Sánchez <Manuss20@gmail.com>
2023-05-15 21:49:38 +0000

    Update day

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-             $('#defaultCountdown').countdown({until: new Date(2023, 5, 27, 8)}); // year, month, date, hour
+             $('#defaultCountdown').countdown({until: new Date(2023, 4, 27, 8)}); // year, month, date, hour
         });
 });		
 
