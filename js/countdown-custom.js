ab418340946c96bc98f0dd70c0b9ff227fad0f8d
Robert Bermejo <bermejoblasco@live.com>
2024-01-28 19:38:35 +0100

    web 2024

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-             $('#defaultCountdown').countdown({until: new Date(2023, 10, 17, 8)}); // year, month, date, hour
+             $('#defaultCountdown').countdown({until: new Date(2024, 2, 23, 8)}); // year, month, date, hour
         });
 });		
 
